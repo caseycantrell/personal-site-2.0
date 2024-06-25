9a9acf637bf964246d1708aa38198f76fafb5748
Casey Cantrell <cantrellcasey@gmail.com>
2024-06-25 17:30:48 -0500

    styling fix

--- src/app/Components/ClickyBox.tsx
+++ src/app/Components/ClickyBox.tsx
@@ -92,7 +92,7 @@ const ClickyBox: React.FC = () => {
           .square {
             width: 100vw;
             height: 100vh;
-            perspective: 2000px;
+            perspective: 20000px;
           }
         }
         @media (min-width: 1024px) {
