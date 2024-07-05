82d2ed5b587865112a2688abd38e07590d184d11
Casey Cantrell <cantrellcasey@gmail.com>
2024-07-05 14:24:37 -0500

    text size

--- src/app/Components/UI/Copyright.tsx
+++ src/app/Components/UI/Copyright.tsx
@@ -1,7 +1,7 @@
 const Copyright = () => {
     return (
       <div className="flex flex-row items-center absolute bottom-2 right-4">
-        <p className="font-semibold text-white opacity-50 text-xs lg:text-lg">&copy; 2024 Casey Cantrell. All rights reserved.</p>
+        <p className="font-semibold text-white opacity-50 text-xs lg:text-md">&copy; 2024 Casey Cantrell. All rights reserved.</p>
       </div>
     )
   } 
