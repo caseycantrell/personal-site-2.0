c424c5092a4890cfcbd0dc997d31e9e7467aeb30
Casey Cantrell <cantrellcasey@gmail.com>
2024-07-05 14:48:39 -0500

    updates breakpoint defaults and fixes cr on mobile

--- src/app/Components/UI/Copyright.tsx
+++ src/app/Components/UI/Copyright.tsx
@@ -1,6 +1,6 @@
 const Copyright = () => {
     return (
-      <div className="flex flex-row items-center absolute bottom-2 right-4">
+      <div className="hidden lg:flex flex-row items-center absolute bottom-2 right-4">
         <p className="font-semibold text-white opacity-50 text-xs lg:text-md">&copy; 2024 Casey Cantrell. All rights reserved.</p>
       </div>
     )
