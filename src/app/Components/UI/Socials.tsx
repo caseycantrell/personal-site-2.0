ac98198c5937d58b8be3aa7d9f0e6425498a7e15
Casey Cantrell <cantrellcasey@gmail.com>
2024-07-18 15:17:23 -0500

    fixes fade

--- src/app/Components/UI/Socials.tsx
+++ src/app/Components/UI/Socials.tsx
@@ -2,29 +2,14 @@ import Image from "next/image"
 import { motion } from 'framer-motion'
 import { hoverTransition } from "./Animations"
 
-const containerVariants = {
-  initial: {
-    x: 100
-  },
-  animate: {
-    x: 0,
-    transition: {
-      staggerChildren: -0.2,
-      delayChildren: 0.5
-    }
-  }
-}
-
 const fadeIn = {
-  initial: { opacity: 0, x: 20 },
+  initial: { opacity: 0 },
   animate: { 
-    opacity: 1, 
-    x: 0,
+    opacity: 1,
     transition: { 
-      duration: 0.5,
       opacity: { 
         duration: 1, ease: "easeInOut",
-        delay: 0.2
+        delay: 0.25
       }
     }
   }
@@ -32,9 +17,7 @@ const fadeIn = {
 
 const Socials = () => {
   return (
-    <motion.div  initial="initial"
-    animate="animate"
-    variants={containerVariants} className="absolute top-4 right-4 hidden lg:flex flex-row items-center gap-x-3.5">
+    <div className="absolute top-4 right-4 hidden lg:flex flex-row items-center gap-x-3.5">
       <motion.a variants={fadeIn} key={1} whileHover={{ scale: 1.125 }} transition={hoverTransition} href={"mailto:cantrellcasey@gmail.com"} className="cursor-pointer">
         <Image src={'/icons/email.png'} width={33} height={33} alt='Email'/>
       </motion.a>
@@ -44,7 +27,7 @@ const Socials = () => {
       <motion.a variants={fadeIn}  key={3} whileHover={{ scale: 1.125 }} transition={hoverTransition} href={"https://github.com/caseycantrell"} target="_blank" className="cursor-pointer">
         <Image src={'/icons/github.png'} width={33} height={33} alt='Github' />
       </motion.a>
-    </motion.div>
+    </div>
   )
 }
   
